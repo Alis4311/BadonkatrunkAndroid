5bb529e0caa87847fdab77d2e490613b1e06555c
Tim Normark <timnormark@gmail.com>
2018-03-13 09:18:09 +0100

    Tims enda bidrag till spelet

---
+++ app/src/main/java/se/mah/ag4431/badonkatrunkandroid/Entities/TimPlayer.java
@@ -0,0 +1,11 @@
+package se.mah.ag4431.badonkatrunkandroid.Entities;
+
+/**
+ * Created by timnormark on 2018-03-13.
+ */
+
+public class TimPlayer extends GameObject {
+    public void play() {
+        System.out.println("Jag spelar!");
+    }
+}
